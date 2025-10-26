8d0b3432c5cb05f51e679e00e31506091508551e
George Apostolopoulos <gapost@ipta.demokritos.gr>
2025-10-26 11:22:12 +0200

    Correct bug in qdatasliceslector

--- src/qdatasliceselector.cpp
+++ src/qdatasliceselector.cpp
@@ -13,7 +13,7 @@
 QDataSliceSelector::QDataSliceSelector(QWidget *parent)
     : QWidget{parent}
 {
-    //setStyleSheet("border: 1px solid black");
+    // setStyleSheet("border: 1px solid black");
 
     // layout
     QVBoxLayout *vbox = new QVBoxLayout;
@@ -53,7 +53,7 @@ QDataSliceSelector::QDataSliceSelector(QWidget *parent)
         btExchangeXY = new QToolButton;
         btExchangeXY->setIcon(QIcon(":/qdatabrowser/lucide/arrow-left-right.svg"));
         btExchangeXY->setAutoRaise(true);
-        //btExchangeXY->setEnabled(false);
+        // btExchangeXY->setEnabled(false);
         btExchangeXY->setToolTip("X ↔ Y");
         hbox->addWidget(btExchangeXY);
 
@@ -73,7 +73,7 @@ QDataSliceSelector::QDataSliceSelector(QWidget *parent)
     /* create grid widget*/
     {
         gridPanel = new QWidget;
-        //gridPanel->setStyleSheet("border: 1px solid black");
+        // gridPanel->setStyleSheet("border: 1px solid black");
 
         grid = new QGridLayout;
         grid->setContentsMargins(0, 0, 0, 0);
@@ -117,11 +117,13 @@ void QDataSliceSelector::clearCtrls()
     cbY->setEnabled(false);
     btExchangeXY->setEnabled(false);
     QLayoutItem *i;
-    while ((i = grid->takeAt(0)) != nullptr) {
+    while ((i = grid->takeAt(0)) != nullptr)
+    {
         delete i;
     }
 
-    for (auto &e : gridElements) {
+    for (auto &e : gridElements)
+    {
         e.label->deleteLater();
         e.slider->deleteLater();
         e.value->deleteLater();
@@ -141,15 +143,18 @@ void QDataSliceSelector::initCtrls()
     int slice_dim = slice_.ndim();
     int nrows = std::max(ndim - slice_dim, 0);
 
-    for (int i = 0; i < ndim; ++i) {
+    for (int i = 0; i < ndim; ++i)
+    {
         cbX->addItem(dimLabel(i));
     }
     cbX->setCurrentIndex(slice_.dx());
 
-    if (slice_dim == 2) {
+    if (slice_dim == 2)
+    {
         cbY->setEnabled(true);
         btExchangeXY->setEnabled(true);
-        for (int i = 0; i < ndim; ++i) {
+        for (int i = 0; i < ndim; ++i)
+        {
             cbY->addItem(dimLabel(i));
         }
         cbY->setCurrentIndex(slice_.dy());
@@ -160,7 +165,8 @@ void QDataSliceSelector::initCtrls()
 
     gridElements.resize(nrows);
     int k = ndim - nrows;
-    for (int r = 0; r < nrows; ++r, ++k) {
+    for (int r = 0; r < nrows; ++r, ++k)
+    {
         gridElement &e = gridElements[r];
         e.d = slice_.dim_order()[k];
         e.label = new QLabel(dimLabel(e.d));
@@ -184,7 +190,8 @@ void QDataSliceSelector::updateCtrls(updFlag f)
     if (slice_.empty())
         return;
 
-    if (f == XYex) {
+    if (f == XYex)
+    {
         cbX->setCurrentIndex(slice_.dx());
         cbY->setCurrentIndex(slice_.dy());
         return;
@@ -194,40 +201,50 @@ void QDataSliceSelector::updateCtrls(updFlag f)
     auto &dim_order = slice_.dim_order();
     int d0 = slice_.ndim();
 
-    switch (f) {
+    switch (f)
+    {
     case All:
         cbX->setCurrentIndex(slice_.dx());
         if (cbY->isEnabled())
             cbY->setCurrentIndex(slice_.dy());
         // remake sliders
-        for (int i = 0; i < gridElements.size(); ++i) {
+        for (int i = 0; i < gridElements.size(); ++i)
+        {
             auto &e = gridElements[i];
             e.d = dim_order[d0 + i];
             e.label->setText(dimLabel(e.d));
             size_t n = slice_.dataStore()->dim()[e.d];
-            if (n > 1) {
+            if (n > 1)
+            {
                 e.slider->setEnabled(true);
                 e.slider->setRange(0, n - 1);
                 size_t page = 1;
                 size_t nticks = n;
-                while (nticks > maxTicks) {
+                while (nticks > maxTicks)
+                {
                     page++;
                     nticks = n / page;
                 }
                 e.slider->setTickInterval(page);
                 e.slider->setPageStep(page);
-            } else {
+            }
+            else
+            {
                 e.slider->setEnabled(false);
                 e.value->clear();
             }
         }
         setSliderLabels();
     case SldrOnly:
-        for (int i = 0; i < gridElements.size(); ++i) {
+        for (int i = 0; i < gridElements.size(); ++i)
+        {
             auto &e = gridElements[i];
-            size_t k = slice_.i0()[e.d];
-            e.slider->setValue(k);
-            e.value->setText(e.valueLbls.at(k));
+            if (e.slider->isEnabled())
+            {
+                size_t k = slice_.i0()[e.d];
+                e.slider->setValue(k);
+                e.value->setText(e.valueLbls.at(k));
+            }
         }
     default:
         break;
@@ -245,7 +262,8 @@ void QDataSliceSelector::connectCtrls()
             this,
             &QDataSliceSelector::onY);
     connect(btExchangeXY, &QToolButton::clicked, this, &QDataSliceSelector::onExchangeXY);
-    for (auto &e : gridElements) {
+    for (auto &e : gridElements)
+    {
         connect(e.slider, &QSlider::valueChanged, this, &QDataSliceSelector::onI0);
     }
 }
@@ -261,7 +279,8 @@ void QDataSliceSelector::disconnectCtrls()
                this,
                &QDataSliceSelector::onY);
     disconnect(btExchangeXY, &QToolButton::clicked, this, &QDataSliceSelector::onExchangeXY);
-    for (auto &e : gridElements) {
+    for (auto &e : gridElements)
+    {
         disconnect(e.slider, &QSlider::valueChanged, this, &QDataSliceSelector::onI0);
     }
 }
@@ -289,17 +308,22 @@ void QDataSliceSelector::setSliderLabels()
     if (!D)
         return;
 
-    for (int i = 0; i < gridElements.size(); ++i) {
+    for (int i = 0; i < gridElements.size(); ++i)
+    {
         auto &e = gridElements[i];
-        if (e.slider->isEnabled()) {
+        if (e.slider->isEnabled())
+        {
             e.valueLbls.clear();
             size_t n = D->dim()[e.d];
-            if (D->is_x_categorical(e.d)) {
+            if (D->is_x_categorical(e.d))
+            {
                 AbstractDataStore::strvec_t x(n);
                 D->get_x_categorical(e.d, x);
                 for (size_t i = 0; i < n; ++i)
                     e.valueLbls.push_back(QString("%1: %2").arg(i).arg(x[i].c_str()));
-            } else {
+            }
+            else
+            {
                 AbstractDataStore::vec_t x(n);
                 D->get_x(e.d, x);
                 for (size_t i = 0; i < n; ++i)
@@ -316,15 +340,20 @@ void QDataSliceSelector::onX(int new_dx)
     updFlag f = All;
 
     auto i0 = slice_.i0();
-    if (slice_.ndim() == 1) {
+    if (slice_.ndim() == 1)
+    {
         slice_.assign(slice_.dataStore(), new_dx, i0);
-    } else {
+    }
+    else
+    {
         int dy = slice_.dy();
-        if (dy == new_dx) {
+        if (dy == new_dx)
+        {
             int dx = slice_.dx();
             slice_.assign(slice_.dataStore(), new_dx, dx, i0);
             f = XYex;
-        } else
+        }
+        else
             slice_.assign(slice_.dataStore(), new_dx, dy, i0);
     }
 
@@ -343,11 +372,13 @@ void QDataSliceSelector::onY(int new_dy)
 
     auto i0 = slice_.i0();
     int dx = slice_.dx();
-    if (dx == new_dy) {
+    if (dx == new_dy)
+    {
         int dy = slice_.dy();
         slice_.assign(slice_.dataStore(), dy, new_dy, i0);
         f = XYex;
-    } else
+    }
+    else
         slice_.assign(slice_.dataStore(), dx, new_dy, i0);
 
     updateCtrls(f);
@@ -378,9 +409,11 @@ void QDataSliceSelector::onI0(int v)
     blockCtrls(true);
 
     auto i0 = slice_.i0();
-    for (int i = 0; i < gridElements.size(); ++i) {
+    for (int i = 0; i < gridElements.size(); ++i)
+    {
         gridElement &e = gridElements[i];
-        if (sender() == e.slider) {
+        if (sender() == e.slider)
+        {
             i0[e.d] = v;
             slice_.assign(i0);
             break;
